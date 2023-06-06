0b990f91cb916ba03c9dc56c20a565ab68bae0f0
bilbo <bilbo.derbyshire@gmail.com>
2023-06-06 09:12:56 +0100

    Feature: Create input label

--- main.py
+++ main.py
@@ -11,7 +11,14 @@ class TemperatureConverture(tk.Tk):
         self.iconbitmap(default="app-icon.ico")
 
         self.temp_input = TemperatureInput()
-        self.temp_input.grid(column=0, columnspan=2, row=1, sticky=tk.NSEW, padx=5, pady=5)
+        self.temp_input.grid(column=0, columnspan=2, row=1, sticky=tk.NSEW, padx=5)
+
+        self.input_label = tk.Label(
+            font=(PRIMARY_FAMILY, LABEL_SIZE),
+            text="Temperature (C)",
+            anchor="sw",
+        )
+        self.input_label.grid(column=0, columnspan=2, row=0, sticky=tk.NSEW, padx=5, pady=5)
 
         self.rowconfigure([0, 1, 2], weight=1)
         self.columnconfigure([0, 1, 2], weight=1)
@@ -23,7 +30,7 @@ class TemperatureInput(tk.Entry):
 
         self.default_input_string = "Type your temperature here"
         self.input_variable = tk.StringVar(value=self.default_input_string)
-        self.configure(font=("Segoe UI", 13),
+        self.configure(font=(PRIMARY_FAMILY, PRIMARY_SIZE),
                        textvariable=self.input_variable,
                        foreground=INACTIVE_TEXT_COLOR)
 
--- settings.py
+++ settings.py
@@ -3,6 +3,11 @@
 GEOMETRY = "300x100"
 ROOT_TITLE = "Temperature Converter"
 
+# Fonts
+PRIMARY_FAMILY = "Segoe UI"
+PRIMARY_SIZE = "13"
+LABEL_SIZE = "9"
+
 # Colours
 
 INACTIVE_TEXT_COLOR = "#757575"
