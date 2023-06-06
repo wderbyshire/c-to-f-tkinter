e0b79fd905d139f27fb6d6dd49efdb2d48444638
bilbo <bilbo.derbyshire@gmail.com>
2023-06-06 09:08:09 +0100

    Feature: Create input box

--- main.py
+++ main.py
@@ -10,6 +10,25 @@ class TemperatureConverture(tk.Tk):
         self.title(ROOT_TITLE)
         self.iconbitmap(default="app-icon.ico")
 
+        self.temp_input = TemperatureInput()
+        self.temp_input.grid(column=0, columnspan=2, row=1, sticky=tk.NSEW, padx=5, pady=5)
+
+        self.rowconfigure([0, 1, 2], weight=1)
+        self.columnconfigure([0, 1, 2], weight=1)
+
+
+class TemperatureInput(tk.Entry):
+    def __init__(self):
+        super().__init__()
+
+        self.default_input_string = "Type your temperature here"
+        self.input_variable = tk.StringVar(value=self.default_input_string)
+        self.configure(font=("Segoe UI", 13),
+                       textvariable=self.input_variable,
+                       foreground=INACTIVE_TEXT_COLOR)
+
+
+
 
 if __name__ == "__main__":
     app = TemperatureConverture()
--- settings.py
+++ settings.py
@@ -1,4 +1,9 @@
 # Root app details
 
-GEOMETRY = "500x200"
+GEOMETRY = "300x100"
 ROOT_TITLE = "Temperature Converter"
+
+# Colours
+
+INACTIVE_TEXT_COLOR = "#757575"
+ACTIVE_TEXT_COLOR = "#282828"
