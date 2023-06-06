4b65cbfe44302a20a6bbe91eb081777d9d28d57d
bilbo <bilbo.derbyshire@gmail.com>
2023-06-06 08:08:32 +0100

    Feature: Create settings file

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from settings import *
 
 
 class TemperatureConverture(tk.Tk):
---
+++ settings.py
@@ -0,0 +1,4 @@
+# Root app details
+
+GEOMETRY = "500x200"
+ROOT_TITLE = "Temperature Converter"
