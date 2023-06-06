b6321fe205265fbc29169f1a0a4bf6945a7503cb
bilbo <bilbo.derbyshire@gmail.com>
2023-06-06 08:08:03 +0100

    Feature: Set up root app

---
+++ main.py
@@ -0,0 +1,11 @@
+import tkinter as tk
+
+
+class TemperatureConverture(tk.Tk):
+    def __init__(self):
+        super().__init__()
+
+
+if __name__ == "__main__":
+    app = TemperatureConverture()
+    app.mainloop()
