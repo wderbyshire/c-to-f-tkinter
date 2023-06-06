333fefa88a3f4adf5db1a36b3fc8d7d103488faa
bilbo <bilbo.derbyshire@gmail.com>
2023-06-06 09:32:48 +0100

    Fix: Accidental commit

--- main.py
+++ main.py
@@ -10,32 +10,6 @@ class TemperatureConverture(tk.Tk):
         self.title(ROOT_TITLE)
         self.iconbitmap(default="app-icon.ico")
 
-        self.temp_input = TemperatureInput()
-        self.temp_input.grid(column=0, columnspan=2, row=1, sticky=tk.NSEW, padx=5)
-
-        self.input_label = tk.Label(
-            font=(PRIMARY_FAMILY, LABEL_SIZE),
-            text="Temperature (C)",
-            anchor="sw",
-        )
-        self.input_label.grid(column=0, columnspan=2, row=0, sticky=tk.NSEW, padx=5, pady=5)
-
-        self.rowconfigure([0, 1, 2], weight=1)
-        self.columnconfigure([0, 1, 2], weight=1)
-
-
-class TemperatureInput(tk.Entry):
-    def __init__(self):
-        super().__init__()
-
-        self.default_input_string = "Type your temperature here"
-        self.input_variable = tk.StringVar(value=self.default_input_string)
-        self.configure(font=(PRIMARY_FAMILY, PRIMARY_SIZE),
-                       textvariable=self.input_variable,
-                       foreground=INACTIVE_TEXT_COLOR)
-
-
-
 
 if __name__ == "__main__":
     app = TemperatureConverture()
