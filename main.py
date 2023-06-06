fc4efeade110ced1e736648533cfd5c9ec8743a1
bilbo <bilbo.derbyshire@gmail.com>
2023-06-06 08:08:53 +0100

    Feature: Create settings file

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ class TemperatureConverture(tk.Tk):
     def __init__(self):
         super().__init__()
 
+        self.geometry(GEOMETRY)
+
 
 if __name__ == "__main__":
     app = TemperatureConverture()
