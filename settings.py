a4cd26b22366a18c6d18c8a75e057c5deddee335
bilbo <bilbo.derbyshire@gmail.com>
2023-06-06 10:47:36 +0100

    Feature: Settings

--- settings.py
+++ settings.py
@@ -2,3 +2,13 @@
 
 GEOMETRY = "300x100"
 ROOT_TITLE = "Temperature Converter"
+
+# Fonts
+PRIMARY_FAMILY = "Segoe UI"
+PRIMARY_SIZE = "13"
+LABEL_SIZE = "9"
+
+# Colours
+
+INACTIVE_TEXT_COLOR = "#757575"
+ACTIVE_TEXT_COLOR = "#282828"
